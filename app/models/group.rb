50b572c5573838efc218aa9aa964efb3481ddbc9
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-20 16:50:06 -0500

    Add group unit tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :groupings
   has_many :sessions, through: :groupings, dependent: :nullify
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 
   def to_param
     name
---
+++ spec/models/group_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Group, :type => :model do
+  let!(:test_group) { Group.create(name: "Group")}
+
+  context "validation" do
+    it "ensures name is present" do
+      group = Group.new(icon: "url").save
+      expect(group).to eq(false)
+    end
+
+    it "ensures name is unique" do
+      group = Group.new(name: "Group").save
+      expect(group).to eq(false)
+    end
+  end
+end
