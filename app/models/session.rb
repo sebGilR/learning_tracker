e3ba75a6156e6080d5509b67cd3dc21ac16f5204
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-21 00:46:14 -0500

    Fix session and user validations

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,7 +2,7 @@ class Session < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :groupings, dependent: :destroy
   has_many :groups, through: :groupings
-  validates :name, presence: true, length: { maximum: 10 }
+  validates :name, presence: true, length: { maximum: 20 }
   validates :amount, presence: true, numericality: { only_integer: true }
 
   def group
