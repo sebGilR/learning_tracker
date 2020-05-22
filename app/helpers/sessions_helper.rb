9ddff3d525a1d6c26ce65ca9f318be58fffead64
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-05-22 14:22:47 -0500

    Add scrolling to group list

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,9 @@ module SessionsHelper
   def g_icon(session)
     return if session.group.nil?
 
-    html = image_tag(session.group.icon, class: 'group__thumbnail', onerror: "this.error=null;this.src='https://placeimg.com/50/50/tech'")
+    html = image_tag(session.group.icon,
+                     class: 'group__thumbnail',
+                     sonerror: "this.error=null;this.src='https://placeimg.com/50/50/tech'")
     html.html_safe
   end
 end
